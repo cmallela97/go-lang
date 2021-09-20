4f753fcd70ab37c5044014604fb64c385ced9c6e
cmallela97 <cmallela97@users.noreply.github.com>
2021-09-20 18:13:57 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var name = "Chandra Mallela"
+var cityAndState = "Edison, NJ"
+
+func main() {
+	fmt.Println("My Name is :" , name)
+	fmt.Println("I live in  :" , cityAndState)
+
+}
